a63d9c7ab1d82056ee1f6e727a0fe6340308efef
mynk10q <amit1100941@gmail.com>
2026-03-23 00:19:41 +0530

    Create key-api.js

---
+++ api/key-api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  try {
+
+    const { term, key } = req.query;
+
+    if (!term || !key) {
+      return res.status(400).json({
+        status: false,
+        message: "Missing parameters"
+      });
+    }
+
+    const url =
+      `https://familyyyy-info.vercel.app/key-api?key=${key}&term=${term}`;
+
+    const response = await axios.get(url);
+
+    const data = { ...response.data };
+
+    delete data.dev_credit;
+    delete data.credit;
+
+    return res.status(200).json({
+      ...data,
+      dev_credit: "@mynk_mynk_mynk",
+      credit: "@mynk_mynk_mynk"
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: "API Down"
+    });
+  }
+}
